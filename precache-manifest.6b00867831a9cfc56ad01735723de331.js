24c790e692a5e9aac1b8dee4e409cfa0fc7e0900
Billal Hossain <proffesergio@gmail.com>
2020-07-13 15:32:04 +0000

    Updates

---
+++ precache-manifest.6b00867831a9cfc56ad01735723de331.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae1b965dcd3debea0c3e2246aeaf84b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd37953f321341fed769",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a6c22b93765153ea2710",
+    "url": "/static/js/2.26e4a980.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26e4a980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd37953f321341fed769",
+    "url": "/static/js/main.1d819df2.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "ab50a9be7c8b1d9a600681f51ce4e77e",
+    "url": "/static/media/profile.ab50a9be.jpg"
+  }
+]);
